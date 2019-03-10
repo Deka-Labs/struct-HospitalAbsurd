4694bc249908cd34f32c22666e491c34531dc8e2
SpeedCrash100 <SpeedCrash100@yandex.ru>
2019-03-10 08:01:32 +0300

    Basic DoctorBinTree model

---
+++ lib/models/doctorbintree.cpp
@@ -0,0 +1,96 @@
+#include "doctorbintree.hpp"
+
+DoctorBinTree::DoctorBinTree(QObject* parent)
+    : QAbstractTableModel(parent)
+    , m_binTree()
+{
+}
+
+DoctorBinTree::~DoctorBinTree()
+{
+}
+
+QVariant DoctorBinTree::headerData(int section, Qt::Orientation orientation, int role) const
+{
+    if (orientation == Qt::Vertical) {
+        if (role == Qt::DisplayRole) {
+            switch (section) {
+            case 0:
+                return "Фамилия, инициалы";
+            case 1:
+                return "Должность";
+            case 2:
+                return "Кабинет";
+            case 3:
+                return "Расписание";
+            }
+        }
+    }
+    return QVariant();
+}
+
+QVariant DoctorBinTree::data(const QModelIndex& index, int role) const
+{
+    if (!index.isValid())
+        return false;
+
+    if (role == Qt::DisplayRole) {
+        Doctor doc = m_listToDisplay.at(static_cast<unsigned>(index.row()));
+        switch (index.column()) {
+        case 0:
+            return doc.fullname();
+        case 1:
+            return doc.post();
+        case 2:
+            return doc.cabinet();
+        case 3:
+            return doc.schedule();
+        default:
+            return "ERROR";
+        }
+    }
+
+    return QVariant();
+}
+
+int DoctorBinTree::rowCount(const QModelIndex& parent) const
+{
+    (void)parent;
+    return static_cast<int>(m_listToDisplay.size());
+}
+
+int DoctorBinTree::columnCount(const QModelIndex& parent) const
+{
+    (void)parent;
+    //ФИО, Должность, кабинет, расписание 4 - столбца
+    return 4;
+}
+
+bool DoctorBinTree::addDoctor(const Doctor& other)
+{
+    if (m_binTree.add(other)) {
+        updateList();
+        return true;
+    }
+
+    return false;
+}
+
+void DoctorBinTree::removeDoctor(const QModelIndex& index)
+{
+    if (index.isValid()) {
+        Doctor doc = m_listToDisplay.at(static_cast<unsigned>(index.row()));
+        m_binTree.remove(doc.key());
+        updateList();
+    }
+}
+
+void DoctorBinTree::updateList()
+{
+    beginResetModel();
+
+    m_listToDisplay = m_binTree.getListInOrder();
+    m_listToDisplay.sort();
+
+    endResetModel();
+}
---
+++ lib/models/doctorbintree.hpp
@@ -0,0 +1,33 @@
+#ifndef DOCTORBINTREE_HPP
+#define DOCTORBINTREE_HPP
+
+#include "data/bintree.hpp"
+#include "data/doctor.hpp"
+#include "data/twowaylist.hpp"
+#include <QAbstractTableModel>
+
+class DoctorBinTree : public QAbstractTableModel {
+    Q_OBJECT
+private:
+    BinTree<Doctor, QString> m_binTree;
+    TwoWayList<Doctor> m_listToDisplay;
+
+public:
+    explicit DoctorBinTree(QObject* parent = nullptr);
+    DoctorBinTree(const DoctorBinTree&) = delete;
+    ~DoctorBinTree();
+
+    QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const;
+    QVariant data(const QModelIndex& index, int role = Qt::DisplayRole) const;
+
+    int rowCount(const QModelIndex& parent = QModelIndex()) const;
+    int columnCount(const QModelIndex& parent = QModelIndex()) const;
+
+    bool addDoctor(const Doctor& other);
+    void removeDoctor(const QModelIndex& index);
+
+private:
+    void updateList();
+};
+
+#endif // DOCTORBINTREE_HPP
